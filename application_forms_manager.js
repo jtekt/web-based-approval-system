36ed96048425a4698e3aee92ec9e5f6c16c9e50b
Maxime Moreillon <m.moreillon@gmail.com>
2020-04-15 02:53:23 +0000

    dirty fix of file download

--- application_forms_manager.js
+++ application_forms_manager.js
@@ -13,8 +13,8 @@ const axios = require('axios')
 // Custom modules
 const secrets = require('./secrets');
 
-const uploads_directory_path = path.join(__dirname, 'uploads') // for PM2 / Nodemon
-//const uploads_directory_path = path.join("/usr/share/pv", 'afm_uploads') // for building
+//const uploads_directory_path = path.join(__dirname, 'uploads') // for PM2 / Nodemon
+const uploads_directory_path = path.join("/usr/share/pv", 'afm_uploads') // for building
 
 
 const port = 9723
@@ -657,7 +657,7 @@ app.post('/file_upload',check_authentication, (req, res) => {
 });
 
 app.get('/file', (req, res) => {
-
+// UNPROTECTED!
 if(!('id' in req.query)) return res.status(400).send('ID not specified')
 
 var directory_path = path.join(uploads_directory_path, req.query.id)
