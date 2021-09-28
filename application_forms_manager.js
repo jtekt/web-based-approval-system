56c0c1b44a81d23ea0f2a01b100b2f7b8e32523a
Maxime Moreillon <m.moreillon@gmail.com>
2021-09-28 10:57:01 +0900

    root route

--- application_forms_manager.js
+++ application_forms_manager.js
@@ -2,12 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const pjson = require('./package.json')
 const apiMetrics = require('prometheus-api-metrics')
+const {version, author, name: application_name} = require('./package.json')
 
 dotenv.config()
 
-process.env.TZ = 'Asia/Tokyo'
+console.log(`- Application form manager v${version} -`)
+
+process.env.TZ = process.env.TZ || 'Asia/Tokyo'
 
 const port = process.env.APP_PORT || 80
 
@@ -17,12 +19,11 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(apiMetrics())
 
-
 app.get('/', (req, res) => {
   res.send({
-    application_name: 'Shinsei Manager API',
-    author: 'Maxime MOREILLON',
-    version: pjson.version,
+    application_name,
+    author,
+    version,
     neo4j_url: process.env.NEO4J_URL,
     authentication_api_url: process.env.AUTHENTICATION_API_URL,
   })
@@ -31,4 +32,4 @@ app.get('/', (req, res) => {
 app.use('/', require('./routes/v1/index.js'))
 app.use('/v2', require('./routes/v2/index.js'))
 
-app.listen(port, () => console.log(`Application form manager listening on port ${port}`))
+app.listen(port, () => console.log(`[Express] listening on port ${port}`))
