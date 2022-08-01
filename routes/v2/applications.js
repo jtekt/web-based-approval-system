8dd31d4d8279d072568e4e1282f2271360b2d1be
Maxime Moreillon <m.moreillon@gmail.com>
2022-08-01 12:48:20 +0900

    api v2 application create

--- routes/v2/applications.js
+++ routes/v2/applications.js
@@ -2,13 +2,15 @@
 
 const {Router} = require('express')
 const {
-  read_applications
+  create_application,
+  read_applications,
 } = require('../../controllers/v2/applications.js')
 
 const router = Router({mergeParams: true})
 
 
 router.route('/')
+  .post(create_application)
   .get(read_applications)
 
 router.use('/:application_id', require('./application'))
