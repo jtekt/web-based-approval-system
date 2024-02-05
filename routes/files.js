8fb58de449e63a563c992a1f1122b5359e5c6d4c
Maxime Moreillon <m.moreillon@gmail.com>
2024-02-05 11:59:35 +0900

    fixing router

--- routes/files.js
+++ routes/files.js
@@ -1,22 +1,10 @@
 const { Router } = require("express")
-const {
-  file_upload,
-  get_unused_files,
-  move_unused_files,
-  get_file,
-  get_file_name,
-} = require("../controllers/files.js")
+const { file_upload, get_file } = require("../controllers/files.js")
 
 const router = Router({ mergeParams: true })
 
 router.route("/").post(file_upload)
 
 router.route("/:file_id").get(get_file)
-
-router.route("/:file_id/filename").get(get_file_name)
-
-// router.route('/unused')
-//   .get(get_unused_files)
-//   .delete(move_unused_files)
 
 module.exports = router
